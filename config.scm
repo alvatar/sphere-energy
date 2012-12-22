ae2d6c6c7d8e86691bd18cdbc303a2a8b339f329
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-12-22 11:44:33 +0100

    cleaned up config.scm and test dir

--- config.scm
+++ config.scm
@@ -1,13 +1,5 @@
 (sphere: "energy")
 (dependencies:
- ((= exception)
-  (include
-   (energy: exception-macros))
-  (load
-   (algorithm: list)))
  ((= testing-macros)
   (include
-   (energy: exception-macros)))
- ((= testing)
-  (include
-   (energy: testing-macros))))
+   (energy: exception-macros))))
